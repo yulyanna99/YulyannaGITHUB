dcbdc406efa8fcd259c3ffe3d536ffb729ea6e0b
yulyanna99 <65322776+yulyanna99@users.noreply.github.com>
2020-05-13 22:50:09 -0400

    Add files via upload

---
+++ entradaysalida_de_datos.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+///Entradas y salidas de datos
+
+/* SALIDAS DE DATOS MULTIPLES
+int main()
+{
+	int a = 10;
+	float b = 15.5;
+	char c = 'e';
+	printf("%d %f %c", a, b, c);
+	
+	
+	
+	return 0;
+}
+
+ */
+
+int main ()
+{
+	int a;
+	float b;
+	char c;
+	
+	printf("Digite el valor de la variable a, dato entero");
+	scanf("%i", &a);
+	printf("El valor es: %i", a);
+	
+	return 0;
+}
